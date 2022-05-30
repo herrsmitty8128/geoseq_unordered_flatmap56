94cd9ab3ee2a1eb25de32cdc93c4fdbfd59607a3
herrsmitty8128 <16559135+herrsmitty8128@users.noreply.github.com>
2022-05-30 11:23:14 -0400

    license insert

--- geoseq_benchmark.cpp
+++ geoseq_benchmark.cpp
@@ -1,4 +1,9 @@
 
+//          Copyright Christopher Smith 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include <benchmark/benchmark.h>
 #include "ska/bytell_hash_map.hpp"
 #include "geoseq_unordered_flatmap56.h"
